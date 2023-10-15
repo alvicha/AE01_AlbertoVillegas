f9f67f67241af81ca6a7f639465c349e02b5ffbc
Alberto Villegas <alvich@floridauniversitaria.es>
2023-10-15 20:11:53 +0200

    Segon commit AE01

--- src/Activitat/ComparatorLongitut.java
+++ src/Activitat/ComparatorLongitut.java
@@ -9,6 +9,16 @@ import java.util.Comparator;
  * 
  */
 public class ComparatorLongitut implements Comparator<File> {
+	/**
+	 * Mètode per a comparar dos arxius i ordenar-los adequadament en funció de la
+	 * seua grandària en bytes.
+	 * 
+	 * @param file1 Nom del primer arxiu
+	 * @param file2 Nom del segon arxiu
+	 * @return Retorna un número negatiu si size1 és menor que size2, sinó si size1
+	 *         és major que size2, un número positiu. I si són iguals retorna 0.
+	 * 
+	 */
 	@Override
 	public int compare(File file1, File file2) {
 		long size1 = file1.length();
